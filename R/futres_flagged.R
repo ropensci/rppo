bab652a8bf85af0bd54d6930fd3747ffa9b1a9eb
Meghan Balk <meghan.balk@gmail.com>
2021-08-19 11:34:25 -0700

    Update futres_flagged.R

--- R/futres_flagged.R
+++ R/futres_flagged.R
@@ -1,1 +1,10 @@
 #create function to flag
+
+#pass to fuction my_data_frame or r2$data (from previous futres_data() function:
+#' @examples
+#'
+#' r1 <- futres_data(genus = "Puma", limit=10)
+#'
+#' r2 <- futres_data(fromYear = 2009, toYear  = 2018, bbox="37,-120,38,-119", limit=10)
+#'
+#' my_data_frame <- r2$data
