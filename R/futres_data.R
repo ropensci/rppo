55a65c71912d9fb95bb75670b80f313a2f13c7e8
John Deck <jdeck88@gmail.com>
2021-08-30 11:34:16 -0700

    adding ability to get ALL of FuTRES in a single call

--- R/futres_data.R
+++ R/futres_data.R
@@ -47,57 +47,54 @@
 #'
 #' r2 <- futres_data(fromYear = 2009, toYear  = 2018, bbox="37,-120,38,-119", limit=10)
 #'
+#' r_all_futres <- futres_data()
+#'
 #' my_data_frame <- r2$data
 
-futres_data <- function(
-  genus = NULL,
-  specificEpithet = NULL,
-  termID = NULL,
-  fromYear = NULL,
-  toYear = NULL,
-  country = NULL,
-  scientificName = NULL,
-  bbox = NULL,
-  limit = NULL) {
+# set the base_url for making calls
+base_url_all <-
+  "https://biscicol.org/futresapi/downloadable/futres.zip"
+base_url_partial <- "https://biscicol.org/futresapi/v3/download/"
 
-  # source Argument refers to the fields we want returned
-  sourceArgument <-
-    "source=latitude,longitude,yearCollected,termID"
-  # set the base_url for making calls
-  base_url <- "https://www.plantphenology.org/futresapi/v2/download/"
+# Query all FuTRES data
+futres_query_all <- function() {
+  message("this may take awhile... time for some coffee?")
+  return(futres_query(base_url_all))
+}
 
-  # Check for minimum arguments to run a query
-  main_args <- Filter(
-    Negate(is.null),
-    (as.list(c(genus, specificEpithet, termID, bbox, country, scientificName))))
-  date_args <-  Filter(
-    Negate(is.null),
-    (as.list(c(fromYear, toYear))))
-  arg_lengths <- c(length(main_args), length(date_args))
-  if (any(arg_lengths) < 1) {
-    stop("Please specify at least 1 query argument")
-  }
-  userParams <- Filter(
-    Negate(is.null),
-    (as.list(
-      c(
-        genus = genus,
-        specificEpithet = specificEpithet,
-        termID = termID,
-        bbox = bbox,
-        fromYear = fromYear,
-        toYear = toYear,
-        country = country,
-        scientificName = scientificName
-    ))))
-
-  # construct the value following the "q" key
+# Query some FuTRES data
+futres_query_partial <- function(genus = NULL,
+                                 specificEpithet = NULL,
+                                 termID = NULL,
+                                 fromYear = NULL,
+                                 toYear = NULL,
+                                 country = NULL,
+                                 scientificName = NULL,
+                                 bbox = NULL,
+                                 limit = NULL) {
+  # JBD: not sure if this argument is necessary
+  sourceArgument <- "source=latitude,longitude,yearCollected,termID"
   q <- "q="
+  userParams <- Filter(Negate(is.null),
+                       (as.list(
+                         c(
+                           genus = genus,
+                           specificEpithet = specificEpithet,
+                           termID = termID,
+                           bbox = bbox,
+                           fromYear = fromYear,
+                           toYear = toYear,
+                           country = country,
+                           scientificName = scientificName
+                         )
+                       )))
+
+
   # counter to tell us if we're after 1st record
   counter <- 0
   # loop through all user parameters
-  for(key in names(userParams)){
-    value<-userParams[key]
+  for (key in names(userParams)) {
+    value <- userParams[key]
     # For multiple arguments, insert AND separator.  Here, we insert html
     # encoding + for spaces
     if (counter > 0) {
@@ -105,12 +102,11 @@ futres_data <- function(
     }
 
     if (key == "fromYear")
-      q <- paste(q,'%2B','yearCollected:>=',value, sep = "")
+      q <- paste(q, '%2B', 'yearCollected:>=', value, sep = "")
     else if (key == "toYear")
-      q <- paste(q,'%2B','yearCollected:<=',value, sep = "")
+      q <- paste(q, '%2B', 'yearCollected:<=', value, sep = "")
     else if (key == "termID")
-      q <- paste(
-        q,'%2B','termID',':"',value,'"', sep = "")
+      q <- paste(q, '%2B', 'termID', ':"', value, '"', sep = "")
     else if (key == "bbox") {
       lat1 <- as.numeric(unlist(strsplit(bbox, ","))[1])
       lat2 <- as.numeric(unlist(strsplit(bbox, ","))[3])
@@ -130,14 +126,18 @@ futres_data <- function(
         minLng <- lng1
         maxLng <- lng2
       }
-      q <- paste(q, '%2B', 'decimalLatitude', ':>=', minLat, sep = "")
-      q <- paste(q, '+AND+%2B', 'decimalLatitude', ':<=', maxLat, sep = "")
-      q <- paste(q, '+AND+%2B', 'decimalLongitude', ':>=', minLng, sep = "")
-      q <- paste(q, '+AND+%2B', 'decimalLongitude', ':<=', maxLng, sep = "")
+      q <-
+        paste(q, '%2B', 'decimalLatitude', ':>=', minLat, sep = "")
+      q <-
+        paste(q, '+AND+%2B', 'decimalLatitude', ':<=', maxLat, sep = "")
+      q <-
+        paste(q, '+AND+%2B', 'decimalLongitude', ':>=', minLng, sep = "")
+      q <-
+        paste(q, '+AND+%2B', 'decimalLongitude', ':<=', maxLng, sep = "")
     }
     # Begin arguments using +key:value and html encode the + sign with %2B
     else {
-      q <- paste(q, '%2B', key, ':', gsub(" ","+",value), sep = "")
+      q <- paste(q, '%2B', key, ':', gsub(" ", "+", value), sep = "")
     }
     counter <- counter  + 1
   }
@@ -146,13 +146,18 @@ futres_data <- function(
   #q <- paste(q, '+AND+', sourceParameter, sep="")
 
   # construct the queryURL
-  queryUrl <- paste(base_url, '?', q, '&', sourceArgument, sep="")
+  queryUrl <-
+    paste(base_url_partial, '?', q, '&', sourceArgument, sep = "")
 
   # add the limit
   if (!is.null(limit)) {
-    queryUrl <- paste(queryUrl, '&limit=', limit, sep="")
+    queryUrl <- paste(queryUrl, '&limit=', limit, sep = "")
   }
+  return(futres_query(queryUrl))
+}
 
+# A generic FuTRES query, with queryURL sent as parameter
+futres_query <- function(queryUrl) {
   message ('sending request for data ...')
   message(queryUrl)
 
@@ -161,12 +166,14 @@ futres_data <- function(
   # FOVT data portal returns 204 status code when no results have been found
   if (results$status_code == 204) {
     message ("no results found!")
-    return(list(
-      "data" = NULL,
-      "readme" = NULL,
-      "citation" = NULL,
-      "number_possible" = 0,
-      "status_code" = results$status_code)
+    return(
+      list(
+        "data" = NULL,
+        "readme" = NULL,
+        "citation" = NULL,
+        "number_possible" = 0,
+        "status_code" = results$status_code
+      )
     )
   }
   # FOVT server returns a 200 status code when results have been found with
@@ -177,51 +184,99 @@ futres_data <- function(
 
     # save file to disk
     writeBin(bin, tf)
-    untar(tf)
+
+    # using unzip function compatible with v3 download
+    unzip(tf, exdir = "futres_download")
 
     # data.csv contains all data as comma separated values
-    data <- read.csv(
-      'futres_download/data.csv',header=TRUE)
+    data <- read.csv('futres_download/data.csv', header = TRUE)
     # README.txt contains information about the query and the # of results
-    readme <- readr::read_file(
-      'futres_download/FUTRES_README.txt')
+    readme <- readr::read_file('futres_download/FUTRES_README.txt')
     # citation_and_data_use_policies.txt contains citation information
-    citation <- readr::read_file(
-      'futres_download/futres_citation_and_data_use_policies.txt')
+    citation <- readr::read_file('futres_download/futres_citation_and_data_use_policies.txt')
     # grab the number possble from the readme file, using the
     # cat function and capturing output so we can grep results
     # (server does not return a usable count at this time)
-    numPossible <- strsplit(
-      grep(
-        "total results possible",
-        capture.output(cat(readme)),
-        value=TRUE)
-      , " = ")
+    numPossible <- strsplit(grep("total results possible",
+                                 capture.output(cat(readme)),
+                                 value = TRUE)
+                            ,
+                            " = ")
     # convert string version with commas to an integer
-    numPossible <- as.numeric(gsub(",", "", lapply(numPossible, `[`,2)))
+    numPossible <-
+      as.numeric(gsub(",", "", lapply(numPossible, `[`, 2)))
     unlink(tf)
-    unlink("futres_download/", recursive=TRUE)
+    unlink("futres_download/", recursive = TRUE)
 
-    return(list(
-      "data" = data,
-      "readme" = readme,
-      "citation" = citation,
-      "number_possible" = numPossible,
-      "status_code" = results$status_code)
+    return(
+      list(
+        "data" = data,
+        "readme" = readme,
+        "citation" = citation,
+        "number_possible" = numPossible,
+        "status_code" = results$status_code
+      )
     )
 
   }
   # Something unexpected happened
   else {
-    message(paste("The server encountered an issue processing your
-               request and returned status code = ",results$status_code,
-                  ". If the problem persists contact the author."))
-    return(list(
-      "data"= NULL,
-      "readme" = NULL,
-      "citation" = NULL,
-      "number_possible" = NULL,
-      "status_code" = results$status_code))
+    message(
+      paste(
+        "The server encountered an issue processing your
+               request and returned status code = ",
+        results$status_code,
+        ". If the problem persists contact the author."
+      )
+    )
+    return(
+      list(
+        "data" = NULL,
+        "readme" = NULL,
+        "citation" = NULL,
+        "number_possible" = NULL,
+        "status_code" = results$status_code
+      )
+    )
   }
+}
+# Accessor function
+futres_data <- function(genus = NULL,
+                        specificEpithet = NULL,
+                        termID = NULL,
+                        fromYear = NULL,
+                        toYear = NULL,
+                        country = NULL,
+                        scientificName = NULL,
+                        bbox = NULL,
+                        limit = NULL) {
+  # Check for minimum arguments to run a query
+  main_args <- Filter(Negate(is.null),
+                      (as.list(
+                        c(genus, specificEpithet, termID, bbox, country, scientificName)
+                      )))
+  date_args <-  Filter(Negate(is.null),
+                       (as.list(c(fromYear, toYear))))
+  arg_lengths <- c(length(main_args), length(date_args))
+  if (any(arg_lengths) < 1) {
+    return (futres_query_all())
+  } else {
+    # if any query parameters were passed in then call the following
+    # function which fetches data from elasticsearch
+    return (
+      futres_query_partial(
+        genus,
+        specificEpithet,
+        termID,
+        fromYear,
+        toYear,
+        country,
+        scientificName,
+        bbox,
+        limit
+      )
+    )
+  }
+
 }
 
