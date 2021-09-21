79ef2a297e6cb52f8a6651a5f84e67e7be4759ba
Meghan Balk <meghan.balk@gmail.com>
2021-09-21 15:39:19 -0700

    Update log.normal.flag_data.R

--- R/log.normal.flag_data.R
+++ R/log.normal.flag_data.R
@@ -8,7 +8,8 @@ normal.data <- function(
   n.limit = 3, # minimum number of samples wanted to test for normality; default is 3
   method = c("Extracted with Traiter ; estimated value", "Extracted with Traiter ; estimated value; inferred value"), #data we don't want to include in the test; can change it just showing the default
   status <- c("outlier", "too few records"), #data we don't want to include in the test
-  sigma #value for how many standard deviations from the mean to calculate upper and lower limits
+  sigma, #value for how many standard deviations from the mean to calculate upper and lower limits
+  trait #trait of interest
   )
   {
  
@@ -39,76 +40,53 @@ for(i in 1:length(sp)){
   data$sample.size[data$scientificName == sp[i] &
                    data$measurementType == trait &                   
                    data$lifeStage == stage |
+                   data$ageValue >= age &
                    !(data$measurementMethod %in% method &
-                   !(data$measurementStatus %in% status) &
-                   data$ageValue >= age] <- nrow(sub)
+                   !(data$measurementStatus %in% status)] <- nrow(sub)
 }
   
 data$measurementStatus[data$sample.size < n.limit] <- "too few records" #identify which you won't be testing based on sample sizef
 
-for(i in 1:length(sp.transform)){
+for(i in 1:length(sp)){
   #create a subset of data of just the data we want to use to test normality, it will go species by species for the trait selected
    sub <- subset(data, subset = data$scientificName == sp[i] &
+                                data$measurementType == trait &
                                 !(data$measurementStatus %in% status) &
-                                 data$lifeStage == stage |
-                                 data$ageValue >= age &
-                                 !(data$measurementMethod %in% method))
+                                data$lifeStage == stage |
+                                data$ageValue >= age &
+                                !(data$measurementMethod %in% method))
   
-   sub$measurmeentValue <- as.numeric(sub$measurementValue) 
-   sub <- sub[!is.na(sub$measurementValue),]
+   sub$measurmeentValue <- as.numeric(sub$logMeasurementValue) 
+   sub <- sub[!is.na(sub$logMeasurementValue),]
    
-  if(isTRUE(length(sub$measurementValue[sub$measurementType == trait]) < n.limit)){ #if it is less than the sample size limit, then flag as "too few records"
-    data$normality[data$scientificName == sp.transform[i] & 
-                   data$measurementType == trait & 
-                   !(data$measurementStatus %in% status) &
-                   data$lifeStage == age |
-                   data$ageValue >= age &
-                   !(data$measurementMethod %in% method) &
-                   data$normality == "non-normal"] <- "too few records"
+  if(isTRUE(length(sub$logMeasurementValue) < n.limit | 
+            length(unique(sub$logMeasurementValue)){ #if it is less than the sample size limit, then flag as "too few records"
+    data$normality[data$scientificName == sp[i] & 
+                   data$measurementType == trait] <- "too few records"
   }
-  else if(isTRUE(length(sub$measurementValue[sub$measurementType == "{body length}"]) > 5000)){ #if it is greater than 5000, then we need to subsample
-    x <- sample(sub$measurementValue[sub$measurementType == "{body length}"], 
+  else if(isTRUE(length(sub$logMeasurementValue[sub$measurementType == "{body length}"]) > 5000)){ #if it is greater than 5000, then we need to subsample
+    x <- sample(sub$logMeasurementValue[sub$measurementType == "{body length}"], 
                 5000, replace = FALSE, prob = NULL)
-    normal.body.mass <- shapiro.test(x) # normality test
-    if(isTRUE(log.normal.mass[[2]] > 0.05)){ #if not significantly different, then log normal
-      data$normality[data$scientificName == sp.transform[i] & 
-                     data$measurementType == trait & 
-                     !(data$measurementStatus %in% status) &
-                     data$lifeStage == age &|
-                     data$ageValue >= age &
-                     !(data$measurementMethod %in% method) &
-                     data$normality == "non-normal"] <- "log normal"
+    log.normal <- shapiro.test(x) # normality test
+    if(isTRUE(log.normal[[2]] > 0.05)){ #if not significantly different, then log normal
+      data$normality[data$scientificName == sp[i] & 
+                     data$measurementType == trait] <- "log normal"
     }
-    else if(isTRUE(log.normal.mass[[2]] <= 0.05)){ #if significantly different, then non-normal
-      data$normality[data$scientificName == sp.transform[i] & 
-                     data$measurementType == trait & 
-                     !(data$measurementStatus %in% status) &
-                     data$lifeStage == age &|
-                     data$ageValue >= age &
-                     !(data$measurementMethod %in% method) &
-                     data$normality == "non-normal"] <- "non-normal (even logged)"
+    else if(isTRUE(log.normal[[2]] <= 0.05)){ #if significantly different, then non-normal
+      data$normality[data$scientificName == sp[i] & 
+                     data$measurementType == trait] <- "non-log.normal"
     }
   }
-  else if(isTRUE(length(sub$measurementValue[sub$measurementType == trait]) <= 5000 & #if between n.limit and 5000, then test for normality
-                 length(sub$measurementValue[sub$measurementType == trait]) >= n.limit)){
-    log.normal.mass <- shapiro.test(sub$logMeasurementValue[sub$measurementType == trait]) #normality test
-    if(isTRUE(log.normal.mass[[2]] > 0.05)){ #if not significantly different, then log normal
-      data$normality[data$scientificName == sp.transform[i] & 
-                     data$measurementType == trait & 
-                     !(data$measurementStatus %in% status) &
-                     data$lifeStage == age &|
-                     data$ageValue >= age &
-                     !(data$measurementMethod %in% method) &
-                     data$normality == "non-normal"] <- "log normal"
+  else if(isTRUE(length(sub$logMeasurementValue) <= 5000 & #if between n.limit and 5000, then test for normality
+                 length(sub$loMeasurementValue) >= n.limit)){
+    log.normal <- shapiro.test(sub$logMeasurementValue[sub$measurementType == trait]) #normality test
+    if(isTRUE(log.normal[[2]] > 0.05)){ #if not significantly different, then log normal
+      data$normality[data$scientificName == sp[i] & 
+                     data$measurementType == trait] <- "log normal"
     }
-    else if(isTRUE(log.normal.mass[[2]] <= 0.05)){ #if significantly different, non-normal
-      data$normality[data$scientificName == sp.transform[i] & 
-                     data$measurementType == trait & 
-                     !(data$measurementStatus %in% status) &
-                     data$lifeStage == age &|
-                     data$ageValue >= age &
-                     !(data$measurementMethod %in% method) &
-                     data$normality == "non-normal"] <- "non-normal (even logged)"
+    else if(isTRUE(log.normal[[2]] <= 0.05)){ #if significantly different, non-normal
+      data$normality[data$scientificName == sp[i] & 
+                     data$measurementType == trait] <- "non-log.normal"
     }
   }
   else{
@@ -134,22 +112,22 @@ for(i in 1:length(sp){
                                data$lifeStage == stage |
                                data$ageValue >= age &
                                !(data$measurementMethod %in% method) &
-                               data$normality = "non-normal (even logged)") #only for log normal data
+                               data$normality != "non-log.normal") #only for log normal data
   
-   sub$measurmeentValue <- as.numeric(sub$measurementValue) 
-   sub <- sub[!is.na(sub$measurementValue),]
+   sub$logMeasurementValue <- as.numeric(sub$logMeasurementValue) 
+   sub <- sub[!is.na(sub$logMeasurementValue),]
   
   data$meanValue[data$scientificName == sp[i] &
-                 data$measurementType == trait] <- mean(sub$measurementValue) #get mean value for each species and each trait
+                 data$measurementType == trait] <- mean(sub$logMeasurementValue) #get mean value for each species and each trait
   
   data$sdValue[data$scientificName == sp[i] &
-               data$measurementType == trait] <- sd(sub$measurementValue) #get standard deviation for each species and each trait
+               data$measurementType == trait] <- sd(sub$logMeasurementValue) #get standard deviation for each species and each trait
                                                    
   data$upperLimit[data$scientificName == sp[i] &
-                   data$measurementType == trait] <- mean(sub$measurementValue)+sigma*sd(sub$measurementValue) #calculate upper limit as mean + sigma*sd
+                   data$measurementType == trait] <- mean(sub$logMeasurementValue)+sigma*sd(sub$measurementValue) #calculate upper limit as mean + sigma*sd
   
   data$lowerLimit[data$scientificName == sp[i] &
-                  data$measurementType == trait] <- mean(sub$measurementValue)-sigma*sd(sub$measurementValue) #calculate lower limit as mean = sigma*Sd
+                  data$measurementType == trait] <- mean(sub$logMeasurementValue)-sigma*sd(sub$measurementValue) #calculate lower limit as mean - sigma*sd
   
   data$lowerLimitMethod[data$scientificName == sp[i] &
                         data$measurementType == trait] <- "log sd, non-estimated values, no outliers" #label method
@@ -169,7 +147,7 @@ for(i in 1:length(sp)){
       data$measurementStatus[data$index == sub$index[j]] <- "juvenile.log.sd"
     }
     else if(isTRUE(sub$measurementValue[j] >= sub$upperLimit[1])){
-      data$measurementStatus[data$index == sub$index[j]] <- "outlier.log.lsd"
+      data$measurementStatus[data$index == sub$index[j]] <- "outlier.log.sd"
     }
     else{
       data$measurementStatus[data$index == sub$index[j]] <- "possible adult, possibly good"
