a455c65ebcf6b6d8e6c15c1bb128e4ff2f6be6e4
John Deck <jdeck88@gmail.com>
2021-10-27 06:44:27 -0700

    removing examples

--- R/outlier.flag_data.R
+++ R/outlier.flag_data.R
@@ -1,12 +1,3 @@
-#pass to fuction my_data_frame or r2$data (from previous futres_data() function:
-#' @examples
-#'
-#' r1 <- futres_data(genus = "Puma", limit=10)
-#'
-#' r2 <- futres_data(fromYear = 2009, toYear  = 2018, bbox="37,-120,38,-119", limit=10)
-#'
-#' my_data_frame <- r2$data
-
 ## use Mahalanobis Distance test to 
 
 outlier.flag <- function(
