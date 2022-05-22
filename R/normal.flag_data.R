c9a7ae20d57355ccee46eb4c00ecc53821673b8d
John Deck <jdeck88@gmail.com>
2022-05-22 11:28:27 -0700

    updating normal flag data syntax

--- R/normal.flag_data.R
+++ R/normal.flag_data.R
@@ -47,7 +47,7 @@ for(i in 1:length(sp)){
                                !(data$measurementStatus %in% status) &
                                data$lifeStage == stage |
                                data$ageValue >= age &
-                               data$normality != "log normal"))
+                               data$normality != "log normal")
  
    sub$measurmeentValue <- as.numeric(sub$measurementValue) 
    sub <- !is.na(sub)
