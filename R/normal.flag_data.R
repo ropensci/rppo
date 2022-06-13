1c5431863d125e22aa33979546b11cc4b4482ae6
Meghan Balk <meghan.balk@gmail.com>
2022-06-13 16:36:52 -0700

    annotating outlier.flag_data

--- R/normal.flag_data.R
+++ R/normal.flag_data.R
@@ -2,93 +2,104 @@
 
 #test for normality
 normal.flag <- function(
-  data, #dataset to be added
-  stage = "adult", #lifeStage to include
-  age, #in lieu of lifeStage, ageValues to incldue
-  sigma, #value for how many standard deviations from the mean to calculate upper and lower limits
-  n.limit = 3, #minimum number of samples wanted to test for normality (must be greater than 3); default is three
-  #method = c("Extracted with Traiter ; estimated value", "Extracted with Traiter ; estimated value; inferred value"), #data we don't want to include in the test; can change it just showing the default
-  status = c("outlier", "too few records"), #data we don't want to include in the test
-  trait #trait of interest
-  )
-  {
- 
-sp <- unique(data$scientificName)
-if(!isTRUE(data$normality)){
+    data, #dataset to be added
+    stage = NULL, #lifeStage to include
+    sigma, #value for how many standard deviations from the mean to calculate upper and lower limits
+    n.limit = NULL, #minimum number of samples wanted to test for normality (e.g., must be greater than 3); default is 3
+    #method = c("Extracted with Traiter ; estimated value", "Extracted with Traiter ; estimated value; inferred value"), #data we don't want to include in the test; can change it just showing the default
+    taxa = NULL, #by default does all the species in the dataset
+    status = c("outlier", "too few records"), #data we don't want to include in the test; assumes you've already done the "outlier.flag()"
+    trait #trait of interest
+)
+{
+  
+  if(!isTRUE(data$normality)){
     data$normality <- ""
   }
-if(!isTRUE(data$sample.size)){
-     data$sample.size <- ""
+  if(!isTRUE(data$sample.size)){
+    data$sample.size <- ""
   }
   
-data$index <- rownames(data)
-  
-for(i in 1:length(sp)){
-sub <- subset(data, subset = data$scientificName == sp[i] &
-                             data$measurementType == trait &
-                             !(data$measurementStatus %in% status) &
-                             data$lifeStage == stage |
-                             #!(data$measurementMethod %in% method) &
-                             data$ageValue >= age)
-  
-sub$measurmeentValue <- as.numeric(sub$measurementValue) 
-sub <- sub[!is.na(sub$measurementValue),] #removing NA values
-   
-data$sample.size[data$scientificName == sp[i] &
-                 data$measurementType == trait] <- nrow(sub)
-}
-  
-data$measurementStatus[data$sample.size < n.limit] <- "too few records" #identify which species you don't want to include because they don't have enough samples
+  if(!isTRUE(colnames(data) %in% "measurementStatus")){
+    data[, "measurementStatus"] <- ""
+  }
 
-for(i in 1:length(sp)){
-  #create a subset of data of just the data we want to use to test normality, it will go species by species for the trait selected
-  sub <- subset(data, subset = data$scientificName == sp[i] &
-                               data$measurementType == trait &
-                               !(data$measurementStatus %in% status) &
-                               data$lifeStage == stage |
-                               data$ageValue >= age &
-                               data$normality != "log normal")
- 
-   sub$measurmeentValue <- as.numeric(sub$measurementValue) 
-   sub <- !is.na(sub)
-  
-  if(isTRUE(length(sub$measurementValue[sub$measurementType == trait]) < n.limit |
-            length(unique(sub$measurementValue[sub$measurementType == trait])) < n.limit)){ #if it is less than the sample size limit, then flag as "too few records"
-    data$normality[data$scientificName == sp[i] & 
-                   data$measurementType == trait] <- "too few records"
-  }
-  else if(isTRUE(length(sub$measurementValue[sub$measurementType == trait]) > 5000)){ #if it is greater than 5000, then we need to subsample
-    x <- sample(sub$measurementValue[sub$measurementType == trait], 
-                5000, replace = FALSE, prob = NULL)
-    normal <- shapiro.test(x) #normality test
-    if(isTRUE(normal[[2]] < 0.05)){ #if significantly different, then non-normal
-      data$normality[data$measurementType == trait & 
-                     data$scientificName == sp[i]] <- "non-normal"
-    }
-    else if(isTRUE(normal[[2]] >= 0.05)){ #if not significantly different, then non-normal
-      data$normality[data$measurementType == trait & 
-                     data$scientificName == sp[i]] <- "normal"
-    }
-  }
-  else if(isTRUE(length(sub$measurementValue[sub$measurementType == trait]) <= 5000 & #if between n.limit and 5000, then test for normality
-                 length(sub$measurementValue[sub$measurementType == trait]) >= n.limit) & 
-                 length(unique(sub$measurementValue[sub$measurementType == trait])) > 3){
-    normal.total.length <- shapiro.test(sub$measurementValue[sub$measurementType == trait]) #normality test
-    if(isTRUE(normal.total.length[[2]] < 0.05)){ #if significantly different, then not normal
-      data$normality[data$measurementType == trait & 
-                     data$scientificName == sp[i]] <- "non-normal"
-    }
-    else if(isTRUE(normal.total.length[[2]] >= 0.05)){ #if not significantly different, then normal
-      data$normality[data$measurementType == trait & 
-                     data$scientificName == sp[i]] <- "normal"
-    }
+  if(isTRUE(is.null(taxa))){
+    sp <- unique(data[,"scientificName"])
   }
   else{
-    next
+    sp <- taxa
   }
-}
-           
-#create new columns if they don't currently exist
+  
+  data$index <- rownames(data)
+  
+  for(i in 1:length(sp)){
+    trim <- subset(data, subset = data$scientificName == sp[i] &
+                    data$measurementType == trait &
+                    !(data$measurementStatus %in% status))
+    
+    if(!isTRUE(is.null(stage))){
+      trim <- subset(trim, subset = trim[, "lifeStage"] == stage)
+    }
+    
+    sub$measurmentValue <- as.numeric(sub$measurementValue) 
+    sub <- sub[!is.na(sub["measurementValue",]),] #removing NA values
+    
+    data$sample.size[data$scientificName == sp[i] &
+                       data$measurementType == trait] <- nrow(sub)
+  }
+  
+  data$measurementStatus[data$sample.size < n.limit] <- "too few records" #identify which species you don't want to include because they don't have enough samples
+  
+  for(i in 1:length(sp)){
+    #create a subset of data of just the data we want to use to test normality, it will go species by species for the trait selected
+    sub <- subset(data, subset = data$scientificName == sp[i] &
+                    data$measurementType == trait &
+                    !(data$measurementStatus %in% status) &
+                    data$lifeStage == stage |
+                    data$ageValue >= age &
+                    data$normality != "log normal")
+    
+    sub$measurmeentValue <- as.numeric(sub$measurementValue) 
+    sub <- !is.na(sub)
+    
+    if(isTRUE(length(sub$measurementValue[sub$measurementType == trait]) < n.limit |
+              length(unique(sub$measurementValue[sub$measurementType == trait])) < n.limit)){ #if it is less than the sample size limit, then flag as "too few records"
+      data$normality[data$scientificName == sp[i] & 
+                       data$measurementType == trait] <- "too few records"
+    }
+    else if(isTRUE(length(sub$measurementValue[sub$measurementType == trait]) > 5000)){ #if it is greater than 5000, then we need to subsample
+      x <- sample(sub$measurementValue[sub$measurementType == trait], 
+                  5000, replace = FALSE, prob = NULL)
+      normal <- shapiro.test(x) #normality test
+      if(isTRUE(normal[[2]] < 0.05)){ #if significantly different, then non-normal
+        data$normality[data$measurementType == trait & 
+                         data$scientificName == sp[i]] <- "non-normal"
+      }
+      else if(isTRUE(normal[[2]] >= 0.05)){ #if not significantly different, then non-normal
+        data$normality[data$measurementType == trait & 
+                         data$scientificName == sp[i]] <- "normal"
+      }
+    }
+    else if(isTRUE(length(sub$measurementValue[sub$measurementType == trait]) <= 5000 & #if between n.limit and 5000, then test for normality
+                   length(sub$measurementValue[sub$measurementType == trait]) >= n.limit) & 
+            length(unique(sub$measurementValue[sub$measurementType == trait])) > 3){
+      normal.total.length <- shapiro.test(sub$measurementValue[sub$measurementType == trait]) #normality test
+      if(isTRUE(normal.total.length[[2]] < 0.05)){ #if significantly different, then not normal
+        data$normality[data$measurementType == trait & 
+                         data$scientificName == sp[i]] <- "non-normal"
+      }
+      else if(isTRUE(normal.total.length[[2]] >= 0.05)){ #if not significantly different, then normal
+        data$normality[data$measurementType == trait & 
+                         data$scientificName == sp[i]] <- "normal"
+      }
+    }
+    else{
+      next
+    }
+  }
+  
+  #create new columns if they don't currently exist
   if(!(isTRUE(data$upperLimit))){
     data$upperLimit <- ""
     data$lowerLimit <- ""
@@ -99,55 +110,55 @@ for(i in 1:length(sp)){
     data$meanValue <- ""
     data$sdValue <- ""
   }
-                       
-for(i in 1:length(sp)){
-  sub <- subset(data, subset = data$scientificName == sp[i] &
-                               !(data$measurementStatus %in% status) &
-                               data$lifeStage == stage |
-                               data$ageValue >= age &
-                               #!(data$measurementMethod %in% method) &
-                               data$normality != "non-normal") #only for normal data
   
-   sub$measurementValue <- as.numeric(sub$measurementValue) 
-   sub <- sub[!is.na(sub$measurementValue),]
-  
-  data$meanValue[data$scientificName == sp[i] &
-                 data$measurementType == trait] <- mean(sub$measurementValue) #get mean value for each species and each trait
-  
-  data$sdValue[data$scientificName == sp[i] &
-               data$measurementType == trait] <- sd(sub$measurementValue) #get standard deviation for each species and each trait
-                                                   
-  data$upperLimit[data$scientificName == sp[i] &
-                  data$measurementType == trait] <- mean(sub$measurementValue) + sigma*sd(sub$measurementValue) #calculate upper limit as mean + sigma*sd
-                                                                                                        
-  data$lowerLimit[data$scientificName == sp[i] &
-                  data$measurementType == trait] <- mean(sub$measurementValue) - sigma*sd(sub$measurementValue) #calculate lower limit as mean - sigma*sd
-
-data$lowerLimitMethod[data$scientificName == sp[i] &
-                      data$measurementType == trait] <- "sd" #label method
-  
-  data$upperLimitMethod[data$scientificName == sp[i] &
-                        data$measurementType == trait] <- "sd" #label method
-}
- 
-for(i in 1:length(sp)){
-  sub <- data[data$scientificName == sp[i] & 
-              data$measurementType == trait &
-              #!(data$measurementStatus %in% status) &
-              data$lowerLimitMethod == "sd",] #only use for those that use the raw values
-  
-  for(j in 1:nrow(sub)){
-    if(isTRUE(sub$measurementValue[j] <= sub$lowerLimit[1])){
-      data$measurementStatus[data$index == sub$index[j]] <- "juvenile"
-    }
-    else if(isTRUE(sub$measurementValue[j] >= sub$upperLimit[1])){
-      data$measurementStatus[data$index == sub$index[j]] <- "outlier"
-    }
-    else{
-      data$measurementStatus[data$index == sub$index[j]] <- "possible adult, possibly good"
-    }
+  for(i in 1:length(sp)){
+    sub <- subset(data, subset = data$scientificName == sp[i] &
+                    !(data$measurementStatus %in% status) &
+                    data$lifeStage == stage |
+                    data$ageValue >= age &
+                    #!(data$measurementMethod %in% method) &
+                    data$normality != "non-normal") #only for normal data
+    
+    sub$measurementValue <- as.numeric(sub$measurementValue) 
+    sub <- sub[!is.na(sub$measurementValue),]
+    
+    data$meanValue[data$scientificName == sp[i] &
+                     data$measurementType == trait] <- mean(sub$measurementValue) #get mean value for each species and each trait
+    
+    data$sdValue[data$scientificName == sp[i] &
+                   data$measurementType == trait] <- sd(sub$measurementValue) #get standard deviation for each species and each trait
+    
+    data$upperLimit[data$scientificName == sp[i] &
+                      data$measurementType == trait] <- mean(sub$measurementValue) + sigma*sd(sub$measurementValue) #calculate upper limit as mean + sigma*sd
+    
+    data$lowerLimit[data$scientificName == sp[i] &
+                      data$measurementType == trait] <- mean(sub$measurementValue) - sigma*sd(sub$measurementValue) #calculate lower limit as mean - sigma*sd
+    
+    data$lowerLimitMethod[data$scientificName == sp[i] &
+                            data$measurementType == trait] <- "sd" #label method
+    
+    data$upperLimitMethod[data$scientificName == sp[i] &
+                            data$measurementType == trait] <- "sd" #label method
   }
-} 
-                                                                                                        
+  
+  for(i in 1:length(sp)){
+    sub <- data[data$scientificName == sp[i] & 
+                  data$measurementType == trait &
+                  #!(data$measurementStatus %in% status) &
+                  data$lowerLimitMethod == "sd",] #only use for those that use the raw values
+    
+    for(j in 1:nrow(sub)){
+      if(isTRUE(sub$measurementValue[j] <= sub$lowerLimit[1])){
+        data$measurementStatus[data$index == sub$index[j]] <- "juvenile"
+      }
+      else if(isTRUE(sub$measurementValue[j] >= sub$upperLimit[1])){
+        data$measurementStatus[data$index == sub$index[j]] <- "outlier"
+      }
+      else{
+        data$measurementStatus[data$index == sub$index[j]] <- "possible adult, possibly good"
+      }
+    }
+  } 
+  
   return(data)
 }
--- R/outlier.flag_data.R
+++ R/outlier.flag_data.R
@@ -4,8 +4,8 @@ outlier.flag <- function(
     #assume column names match template
   data, #a dataframe, assumed to be in FuTRES format
   trait, #measurementType of interest
-  stage = NULL, 
-  taxa = NULL,
+  stage = NULL, #omits by default
+  taxa = NULL, #by default does all the species in the dataset
   #assume trait is under "measurementType" and an FOVT term
   sample.limit = NULL #limit for number of samples for testing outliers, defaults to 10
   #assumes dataset is already filtered by lifeStage and ageValue
@@ -13,13 +13,14 @@ outlier.flag <- function(
 ) 
 {
   
+  #add in column to record status of the measurment
+  #(e.g., "too few records", "outlier", "")
   if(!isTRUE(colnames(data) %in% "measurementStatus")){
     data[, "measurementStatus"] <- ""
   }
-  else{
-    next
-  }
   
+  #create index of species names to go through
+  #by default goes through all species in the dataset
   if(isTRUE(is.null(taxa))){
     sp <- unique(data[,"scientificName"])
   }
@@ -30,12 +31,16 @@ outlier.flag <- function(
   rownames(data) <- seq(1, nrow(data),1)
   
   for(i in 1:length(sp)){
+    #create a dataframe of just the measurements of the select trait for a species
     trim <- subset(data, subset = data[, "scientificName"] == sp[i] &
                      data[, "measurementType"] == trait)
+    
+    #if they have lifeStage not null, trim the dataset more
     if(!isTRUE(is.null(stage))){
       trim <- subset(trim, subset = trim[, "lifeStage"] == stage)
     }
     
+    #select out only measurement value
     sub <- subset(trim, select = "measurementValue")
     
     #make numeric
@@ -44,6 +49,7 @@ outlier.flag <- function(
     #remove NAs
     sub  <- na.omit(sub)
     
+    #set limit
     if(isTRUE(is.null(sample.limit))){
       n.limit = 10
     }
@@ -52,23 +58,24 @@ outlier.flag <- function(
     }
     
     if(isTRUE(nrow(sub) == 0)){
-      next
+      next #if it is an empty dataframe, do nothing and go to the next species
     }
     else if(isTRUE(length(unique(sub[, "measurementValue"])) <= n.limit | 
-                   nrow(sub) <= n.limit)){
+                   nrow(sub) <= n.limit)){ #TRUE if not enough observations based on sample.limit
       data[, "measurementStatus"][data[, "scientificName"] == sp[i] &
                                     data[, "measurementType"] == trait] <- "too few records"
     }
-    else if(isTRUE(nrow(sub) >= n.limit)){
+    else if(isTRUE(nrow(sub) >= n.limit)){ #if enough observations, do the outlier test
       outlier <- maha(sub, cutoff = 0.95, rnames = FALSE) #do the test
-      index <- names(outlier[[2]])
+      index <- names(outlier[[2]]) #indexing values that are outliers
       if(isTRUE(length(index) != 0)){
-        data[index,"measurementStatus"] <- "outlier"
+        data[index,"measurementStatus"] <- "outlier" #labeling values that are outliers
       }
     }
     else{
       next
     }
   }
-  return(data)
+  return(data) #returns the full dataframe
+  #this means that the traits and species not specified will not have values in "measurementStatus"
 }                
