bfcec4159b3a18501df46fde56b7f88397082d62
Meghan Balk <meghan.balk@gmail.com>
2022-06-13 16:23:51 -0700

    Update outlier.flag_data.R

--- R/outlier.flag_data.R
+++ R/outlier.flag_data.R
@@ -5,14 +5,14 @@ outlier.flag <- function(
   data, #a dataframe, assumed to be in FuTRES format
   trait, #measurementType of interest
   stage = NULL, 
-  sp = NULL,
+  taxa = NULL,
   #assume trait is under "measurementType" and an FOVT term
   sample.limit = NULL #limit for number of samples for testing outliers, defaults to 10
   #assumes dataset is already filtered by lifeStage and ageValue
   #goes through every species in the dataset
 ) 
 {
-
+  
   if(!isTRUE(colnames(data) %in% "measurementStatus")){
     data[, "measurementStatus"] <- ""
   }
@@ -20,12 +20,18 @@ outlier.flag <- function(
     next
   }
   
-  sp <- unique(data[,"scientificName"])
+  if(isTRUE(is.null(taxa))){
+    sp <- unique(data[,"scientificName"])
+  }
+  else{
+    sp <- taxa
+  }
+  
   rownames(data) <- seq(1, nrow(data),1)
   
   for(i in 1:length(sp)){
     trim <- subset(data, subset = data[, "scientificName"] == sp[i] &
-                                  data[, "measurementType"] == trait)
+                     data[, "measurementType"] == trait)
     if(!isTRUE(is.null(stage))){
       trim <- subset(trim, subset = trim[, "lifeStage"] == stage)
     }
@@ -34,7 +40,7 @@ outlier.flag <- function(
     
     #make numeric
     sub[, "measurementValue"] <- as.numeric(sub[, "measurementValue"]) 
-  
+    
     #remove NAs
     sub  <- na.omit(sub)
     
@@ -44,14 +50,14 @@ outlier.flag <- function(
     else{
       n.limit = sample.limit
     }
-
+    
     if(isTRUE(nrow(sub) == 0)){
       next
     }
     else if(isTRUE(length(unique(sub[, "measurementValue"])) <= n.limit | 
                    nrow(sub) <= n.limit)){
       data[, "measurementStatus"][data[, "scientificName"] == sp[i] &
-                                  data[, "measurementType"] == trait] <- "too few records"
+                                    data[, "measurementType"] == trait] <- "too few records"
     }
     else if(isTRUE(nrow(sub) >= n.limit)){
       outlier <- maha(sub, cutoff = 0.95, rnames = FALSE) #do the test
